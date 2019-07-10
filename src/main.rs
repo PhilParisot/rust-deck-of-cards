86a0bb9f3473474d0392534217757ae70c241b0e
Philippe Parisot <pparisot3@gmail.com>
2019-07-09 21:26:39 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    enum Suits {
+        Hearts,
+        Spades,
+        Clubs,
+        Dismonds,
+    }
+
+    struct Card {
+        cardNum: u8,
+        cardSuit: Suits,
+    }
+    
+
+
+    println!("Hello, world!");
+}
